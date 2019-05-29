e2ead9d393030a387958cdd62a9757c791de66f2
alan <info@iscbj.com>
2019-05-29 22:17:15 +0800

    新增 MySQL 备份上传

--- README.md
+++ README.md
@@ -5,3 +5,21 @@
 ```
 python baidu.py file1 file2...
 ```
+
+或上传备份
+
+```
+python baidu.py
+```
+
+已添加
+
+MySQL备份上传
+
+定时备份上传：
+
+```
+chmod +x baidu.py
+# crontab -e
+0 2 * * * /usr/bin/python /path/to/baidu.py
+```
--- baidu.py
+++ baidu.py
@@ -2,15 +2,22 @@
 
 from bypy import ByPy
 import os, sys
-import datetime
+import datetime, time
 
 '''
 1、安装 ByPy：pip install bypy
 2、授权：bypy info
 '''
 
-config = {
-    "backupPath": "backup"
+CONFIG = {
+    "backupPath": "backup", # 百度网盘备份目录名（我的应用数据的bypy 目录下）
+    "db_host": "127.0.0.1", # 数据库主机名
+    "db_user": "root",      # 数据库用户名
+    "db_passwd": "",        # 数据库密码
+    'db_name': [            # 数据库名称列表
+        "abc"
+    ],
+    "mysql_backup": True    # 是否备份
 }
 
 class BackupToBaidu:
@@ -51,7 +58,7 @@ class BackupToBaidu:
 
     def uploadFiles(self, allFiles):
         # 百度云存放文件的文件夹名
-        dir_name = config['backupPath']
+        dir_name = CONFIG['backupPath']
         totalFileSize = 0  # 文件大小变量
         start = datetime.datetime.now()  # 计时开始
 
@@ -59,6 +66,9 @@ class BackupToBaidu:
         bp = ByPy()
         # 百度网盘创建远程文件夹backup
         bp.mkdir(remotepath=dir_name)
+
+        if isinstance(allFiles,str):
+            allFiles = [allFiles]
 
         for file in allFiles:
             if not os.path.exists(file):
@@ -68,11 +78,10 @@ class BackupToBaidu:
             print("正在上传文件:" + file)
 
             if file != "":
-                bp.mkdir(remotepath=dir_name + filePath)
-                DIR_NAME = dir_name + filePath
-                bp.upload(localpath="." + filePath + "/" + fileName, remotepath=str(DIR_NAME), ondup='newcopy')
-                print("文件发送完成：本地路径：" + filePath + "/" + fileName + " 远程文件夹：" + DIR_NAME)
-                totalFileSize += self.get_FileSize("." + filePath + "/" + fileName)
+                localpath = filePath if filePath else "."
+                bp.upload(localpath=localpath + "/" + fileName, remotepath=str(dir_name), ondup='newcopy')
+                print("文件发送完成：本地路径：" + localpath + "/" + fileName + " 远程文件夹：" + dir_name)
+                totalFileSize += self.get_FileSize(localpath + "/" + fileName)
             else:
                 bp.upload(localpath=fileName, remotepath=dir_name, ondup='newcopy')
                 print("文件发送完成：" + fileName + " 远程文件夹：" + dir_name)
@@ -85,9 +94,31 @@ class BackupToBaidu:
         print("花费时间(s)：" + str((end - start).seconds))
         print("\nupload ok")
 
+class AllBackup:
+
+    def mysqlBackup(self):
+        backup_time = time.strftime('%Y%m%d-%H%M%S')
+        for db_name in CONFIG['db_name']:
+            backup_file = "/tmp/" + db_name + backup_time + ".sql"
+            dumpcmd = "mysqldump -u " + CONFIG['db_user'] + " -p" + CONFIG['db_passwd'] + " " + db_name + " > " + backup_file
+            os.system(dumpcmd)
+            print("Dump of "+ backup_file +" completed")
+
+            backup = BackupToBaidu()
+            backup.uploadFiles(backup_file)
+            print("正在删除文件：" + backup_file)
+            os.system("rm -f " + backup_file)
+
+
 
 if __name__ == "__main__":
 
-    allFiles = sys.argv[1:]
-    backup = BackupToBaidu()
-    backup.uploadFiles(allFiles)
+    if len(sys.argv) > 1:
+        allFiles = sys.argv[1:]
+        backup = BackupToBaidu()
+        backup.uploadFiles(allFiles)
+
+    allBackup = AllBackup()
+
+    if CONFIG['mysql_backup']:
+        allBackup.mysqlBackup()
